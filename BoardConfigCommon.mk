c258cab14acd94fae6e03d6c13f920fdcbe5b514
steadfasterX <steadfasterX@users.noreply.github.com>
2025-04-22 11:03:35 +0200

    mk: enable AXP.OS boardconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -230,3 +230,7 @@ WIFI_HIDL_FEATURE_AWARE := true
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# even though we include vendor/axp/config/common.mk we need to include AXP's own BoardConfig
+# (after the above definitions & includes), too so we we can make use of the conditions within
+include vendor/axp/BoardConfigVendor.mk
